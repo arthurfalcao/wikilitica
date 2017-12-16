f94748c27db3459c60a2100d21c3cffc74e54a21
Arthur Falcão <arthurfalcao77@gmail.com>
2017-12-16 03:04:46 -0300

    buggg

--- templates/confedit.php
+++ templates/confedit.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'config.php';
-$id = $_GET['id'];
+$id = $_POST['id'];
 $nome = $_POST['nome'];
 $datanasc = $_POST['data_nasc'];
 $sexo = $_POST['sexo'];
@@ -10,8 +10,12 @@ $funcao = $_POST['funcao'];
 $estado = $_POST['estado'];
 $partido = $_POST['partido'];
 
-$SQL = "UPDATE POLITICO, ESTADO, PARTIDO SET NOME = ?, DATA_NASC = ?, SEXO = ?, PROFISSAO = ?, FUNCAO = ?, ESTADO = ?, PARTIDO = ?WHERE ID_POLITICO = ?";
-        
+$SQL = "UPDATE POLITICO
+        INNER JOIN ESTADO ON ESTADO.ID_ESTADO = POLITICO.ESTADO
+        INNER JOIN PARTIDO ON PARTIDO.ID_PARTIDO = POLITICO.PARTIDO
+        SET POLITICO.NOME = ?, POLITICO.DATA_NASC = ?, POLITICO.SEXO = ?, POLITICO.PROFISSAO = ?, POLITICO.FUNCAO = ?, POLITICO.ESTADO = ?, POLITICO.PARTIDO = ?
+        WHERE ID_POLITICO = ?";
+
 $stmt = $conn->prepare($SQL);
 $stmt->bindParam(1, $nome);
 $stmt->bindParam(2, $datanasc);
@@ -20,8 +24,15 @@ $stmt->bindParam(4, $profissao);
 $stmt->bindParam(5, $funcao);
 $stmt->bindParam(6, $estado);
 $stmt->bindParam(7, $partido);
-$stmt->execute();
-echo "<script>alert('Candidato alterado com sucesso!');</script>";
-echo "<script language=\"javascript\">window.location=\"candidato.php\";</script>";
-?>
+$stmt->bindParam(8, $id);
 
+try {
+  $stmt->execute();
+  echo "<script>alert('Candidato alterado com sucesso!');</script>";
+  echo "<script language=\"javascript\">window.location=\"candidato.php\";</script>";
+} catch (PDOException $e) {
+  echo "Erro ao alterar".$e->getMessage();
+}
+
+
+?>
