92de3c66db8e031101c1872d9f3de6cc387ce283
Rodrigo Camelo <rodrigocamelosilva123@outlook.com>
2017-12-27 18:06:20 -0300

    buscar e imagem cad-perfil

--- templates/mostrar_lista.php
+++ templates/mostrar_lista.php
@@ -46,18 +46,15 @@
 			<img src="../static/img/buscar.png" alt="buscar" class="img-buscar">
 		</a>
 			<form class="cadastroEstilo" method="post" action="<?php $PHP_SELF; ?>">
-				<label for="nome_candidato" class="l1">Nome do Candidato</label>
-				<input id ="nome_candidato" type="text" name="nome_candidato"><br>
-				<label class="l1">Estado de Atuação</label>
-				<select required class="l1" name="estado">
-        	<option>Selecione</option>
+			<input id ="nome_candidato" type="text" name="nome_candidato" placeholder="Nome do Candidato"><br>
+        <select required class="l1" name="estado" id="estado">
+        	<option>Estado</option>
         	<?php while($est = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
           <option value="<?php echo $est['ID_ESTADO'] ?>"><?php echo utf8_encode($est['NOME']) ?></option>
        		<?php } ?>
       	</select><br><br>
-      	<label for="funcao" class="l1">Função</label>
-				<select required class="l1" name="funcao" id="funcao">
-					<option value="" selected="selected">Selecione</option>
+        <select required class="l1" name="funcao" id="funcao">
+					<option value="" selected="selected">Função</option>
           <option>Vereador</option>
 					<option>Prefeito</option>
 					<option>D.Estadual</option>
